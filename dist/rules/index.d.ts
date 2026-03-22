d009386386a5ce723fecdaf8d52b8b59243ab2ff
Tomer Weinstock <tomerwei@checkpoint.com>
2026-03-22 13:02:29 +0200

    fixes

--- dist/rules/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const rules: {
-    "no-sql-injection": import("eslint").Rule.RuleModule;
-    "no-eval": import("eslint").Rule.RuleModule;
-    "no-hardcoded-secrets": import("eslint").Rule.RuleModule;
-    "no-xss": import("eslint").Rule.RuleModule;
-    "no-path-traversal": import("eslint").Rule.RuleModule;
-    "no-command-injection": import("eslint").Rule.RuleModule;
-};
